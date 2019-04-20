eb0279df11e98c86a4f67180c120b9ef131941c5
carmour <callum.m.armour@gmail.com>
2019-04-20 11:58:55 -0700

    comment functionality added

--- Sprint-1.2/javascript/1.2-java.js
+++ Sprint-1.2/javascript/1.2-java.js
@@ -1,23 +1,48 @@
 
 // HOW TO ADD COMMENTS W/ JAVASCRIPT
-var button = document.querySelector('#myButton')
-button.onclick = function() {
-    var someText = document.querySelector('#someText');
-    // console.log(someText.value)
-    var content = document.querySelector('#content')
-    // content.innerText = someText.value
 
-    var newElement = document.createElement('div')
-    newElement.innerText = someText.value + '(' + Date.now() + ')'
-    // there are plenty of great ways to add date, google it
-    newElement.classList.add('item')
-    // this is adding a style to newELement -- I think the class 'item'
-    // needs to exist in our styles sheet?
-    content.appendChild(newElement)
-    // this is adding a new field to our preexisting fields
-
-
-
-    someText.value = '' 
-    // ^this clears the field
+var form = document.getElementById('comments__form');
+form.onsubmit = (e) => {
+    e.preventDefault();
 }
+
+var ourUsers = [
+    {
+        name: "Michael Lyons",
+        comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
+    }, {
+        name: "Gary Wong",
+        comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
+    }, {
+        name: "Theodore Duncan",
+        comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
+    }
+];
+console.log(ourUsers[0].name)
+// ^^ THIS WILL CONSOLE LOG MICHAEL LYONS!!
+
+
+
+var button = document.querySelector('#comments__button');
+button.onclick = () => {
+    var userName = document.querySelector('#userName');
+    var nameContent = document.querySelector('#nameContent');
+    var newName = document.createElement('div');
+    var userComment = document.querySelector('#userComment');
+    var commentContent = document.querySelector('#commentContent');
+    var newComment = document.createElement('div');
+    // push these variables to the array
+    // ourUsers.push(name: userName.value)
+    newName.innerText = ourUsers.i
+    newName.innerText = userName.value
+    // newName.classList.add('name__new')
+    nameContent.appendChild(newName)
+    userName.value = ''
+    newComment.innerText = userComment.value
+    // newComment.classList.add('comments__new')
+    // ^ this will add stylings
+    commentContent.appendChild(newComment)
+    userComment.value = ''
+}
+
+
