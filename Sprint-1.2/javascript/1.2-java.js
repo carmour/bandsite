f7f5113a9e2a9e8d887f8dff39e0be5d2b172ba4
carmour <callum.m.armour@gmail.com>
2019-04-22 02:35:47 -0700

    desktop input comments styled

--- Sprint-1.2/javascript/1.2-java.js
+++ Sprint-1.2/javascript/1.2-java.js
@@ -1,61 +1,41 @@
 
-// HOW TO ADD COMMENTS W/ JAVASCRIPT
-
+// Prevent page from refreshing on comment submit
 var form = document.getElementById('comments__form');
 form.onsubmit = (e) => {
     e.preventDefault();
 }
 
-// function createOriginalComments() {
-    //     for (var i = 0; i < ourUsers.length; i++) {
-        //         var existingName = (ourUsers[i].name);
-        //         // console.log(existingName)
-//         var createName = document.createElement('div');
-//         var ourName = document.querySelector('#ourName');
-//         var existingComment = (ourUsers[i].comment);
-//         // console.log(existingComment)
-//         var createComment = document.createElement('div');
-//         var ourComment = document.querySelector('#ourComment');
-//         createName.innerText = existingName.value;
-//         createComment.innerText = existingComment.value;
-//         ourName.appendChild(createName)
-//         ourComment.appendChild(createComment)
-//     }
-// }
-// createOriginalComments(ourUsers)
-
+// Array of pre-existing comments
 var ourUsers = [
     {
         name: "Michael Lyons",
         comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
         date: "12/18/2018",
-        photo: "bandsite-photos/band-img.jpg"
+        photo: "bandsite-photos/light-grey.png"
     }, {
         name: "Gary Wong",
         comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
         date: "12/12/2018",
-        photo: "bandsite-photos/band-img.jpg"
+        photo: "bandsite-photos/light-grey.png"
 
     }, {
         name: "Theodore Duncan",
         comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
         date: "11/15/2018",
-        photo: "bandsite-photos/band-img.jpg"
+        photo: "bandsite-photos/light-grey.png"
     }
 ];
 
+// Structures comments on bio page base on pre-existing objects,
+// new inputs
 function buildComment(commentInput) {
-    // builds our comment div
-    
     // create photo div
     var photoDiv = document.createElement('div');
     var profilePhoto = document.createElement('img');
     photoDiv.classList.add('photoDiv');
     profilePhoto.classList.add('profilePhoto');
     profilePhoto.src = commentInput.photo;
-    // add photo
-    photoDiv.appendChild(profilePhoto);
-    
+    photoDiv.appendChild(profilePhoto); 
     // create name div
     var nameDiv = document.createElement('div');
     nameDiv.classList.add('nameDiv');
@@ -87,6 +67,7 @@ function buildComment(commentInput) {
     return commentDiv;    
 }
 
+// Iterates through array to create comments
 function commentCreator(commentArray) {
     for (var i = 0; i < commentArray.length; i++) {
         var commentObj = {
@@ -94,25 +75,18 @@ function commentCreator(commentArray) {
             comment: commentArray[i].comment,
             date: commentArray[i].date
         };
-        
-        // commentDiv.innerText = commentObj.name + "\n" + commentObj.date + "\n" + commentObj.comment + "\n"
-        // call buildComment()
         var commentDiv = buildComment(commentArray[i]);
         var commentContent = document.querySelector('#commentContent');
         commentContent.appendChild(commentDiv);    
-
-        // createName.innerText = existingName;
-        // var existingComment = (ourUsers[i].comment);
-        // var createComment = document.createElement('div');
-        // var ourComment = document.querySelector('#ourComment');
-        // // createComment.innerText = existingComment;
-        // ourComment.appendChild(createComment)
     }
 }
 commentCreator(ourUsers)
 
+// Unshift new comments to pre-existing array, add new comments
+// to page
 var button = document.querySelector('#comments__button');
 button.onclick = () => {
+    // takes user input to create new object
     var userName = document.querySelector('#userName');
     var nameContent = document.querySelector('#commentContent');
     var newName = document.createElement('div');
@@ -123,24 +97,20 @@ button.onclick = () => {
     var dd = today.getDate();
     var mm = today.getMonth()+1; 
     var yyyy = today.getFullYear();
+    // creates new object based on new comment
     var newEntry = {
         name: userName.value,
         comment: userComment.value,
         date: mm+'/'+dd+'/'+yyyy,
         photo: "bandsite-photos/Mohan-muruge.jpg"
     }
+    // unshifts new object to array
     ourUsers.unshift(newEntry);
     commentContent.innerText = ''
     commentCreator(ourUsers)
-
-    // newComment.innerText = userName.value + "\n" + userComment.value + "\n";
-    // commentContent.appendChild(newComment);
-    // newName.classList.add('name__new')
-    //nameContent.appendChild(newName)
+    // userName is locked at Mohan Muruge (same w/ image)
     userName.value = 'Mohan Muruge'
-    //newComment.innerText = userComment.value
-    // newComment.classList.add('comments__new')
-    //commentContent.appendChild(newComment)
+    // comment box is cleared after each comment
     userComment.value = ''
 }
 
