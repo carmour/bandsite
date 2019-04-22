b6ead9452bb80fbebbf2cc46d275e44113841f6f
carmour <callum.m.armour@gmail.com>
2019-04-21 21:17:45 -0700

    comment functionality

--- Sprint-1.2/javascript/1.2-java.js
+++ Sprint-1.2/javascript/1.2-java.js
@@ -6,22 +6,10 @@ form.onsubmit = (e) => {
     e.preventDefault();
 }
 
-var ourUsers = [
-    {
-        name: "Michael Lyons",
-        comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
-    }, {
-        name: "Gary Wong",
-        comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
-    }, {
-        name: "Theodore Duncan",
-        comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
-    }
-];
 // function createOriginalComments() {
-//     for (var i = 0; i < ourUsers.length; i++) {
-//         var existingName = (ourUsers[i].name);
-//         // console.log(existingName)
+    //     for (var i = 0; i < ourUsers.length; i++) {
+        //         var existingName = (ourUsers[i].name);
+        //         // console.log(existingName)
 //         var createName = document.createElement('div');
 //         var ourName = document.querySelector('#ourName');
 //         var existingComment = (ourUsers[i].comment);
@@ -36,53 +24,67 @@ var ourUsers = [
 // }
 // createOriginalComments(ourUsers)
 
-// trying to remove some steps from the for loop
-for (var i = 0; i < ourUsers.length; i++) {
-    var existingName = (ourUsers[i].name);
-    var createName = document.createElement('div');
-    var ourName = document.querySelector('#ourName');
-    ourName.appendChild(createName)
-    createName.innerText = existingName.value;
-    var existingComment = (ourUsers[i].comment);
-    var createComment = document.createElement('div');
-    var ourComment = document.querySelector('#ourComment');
-    createComment.innerText = existingComment.value;
-    ourComment.appendChild(createComment)
+var ourUsers = [
+    {
+        name: "Michael Lyons",
+        comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
+    }, {
+        name: "Gary Wong",
+        comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
+    }, {
+        name: "Theodore Duncan",
+        comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
+    }
+];
+
+function commentCreator(commentArray) {
+    for (var i = 0; i < commentArray.length; i++) {
+        var commentObj = {
+            name: commentArray[i].name,
+            comment: commentArray[i].comment
+        };
+        var commentDiv = document.createElement('div');
+        commentDiv.innerText = commentObj.name + "\n" + commentObj.comment + "\n"
+
+        var commentContent = document.querySelector('#commentContent');
+        commentContent.appendChild(commentDiv);    
+
+        // createName.innerText = existingName;
+        // var existingComment = (ourUsers[i].comment);
+        // var createComment = document.createElement('div');
+        // var ourComment = document.querySelector('#ourComment');
+        // // createComment.innerText = existingComment;
+        // ourComment.appendChild(createComment)
+    }
 }
-// createOriginalComments(ourUsers)
-
-
+commentCreator(ourUsers)
 
 var button = document.querySelector('#comments__button');
 button.onclick = () => {
-    var newBox = document.querySelector('#newBox');
-    var newCommentBox = document.createElement('div')
-    newBox.appendChild(newCommentBox)
-
     var userName = document.querySelector('#userName');
-    var nameContent = document.querySelector('#nameContent');
+    var nameContent = document.querySelector('#commentContent');
     var newName = document.createElement('div');
     var userComment = document.querySelector('#userComment');
     var commentContent = document.querySelector('#commentContent');
     var newComment = document.createElement('div');
-    newName.innerText = userName.value
+    var newEntry = {
+        name: userName.value,
+        comment: userComment.value
+    }
+    ourUsers.push(newEntry);
+    commentContent.innerText = ''
+    commentCreator(ourUsers)
+
+    // newComment.innerText = userName.value + "\n" + userComment.value + "\n";
+    // commentContent.appendChild(newComment);
     // newName.classList.add('name__new')
-    nameContent.appendChild(newName)
+    //nameContent.appendChild(newName)
     userName.value = ''
-    newComment.innerText = userComment.value
+    //newComment.innerText = userComment.value
     // newComment.classList.add('comments__new')
-    commentContent.appendChild(newComment)
+    //commentContent.appendChild(newComment)
     userComment.value = ''
 }
 
-// button.onclick = () => {
-//     var userComment = document.querySelector('#userComment');
-//     var commentContent = document.querySelector('#commentContent');
-//     var newComment = document.createElement('div');
-//     newComment.innerText = userComment.value
-//     // newComment.classList.add('comments__new')
-//     commentContent.appendChild(newComment)
-//     userComment.value = ''
-// }
 
 
