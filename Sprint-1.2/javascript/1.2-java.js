bef28981818ab19bd167e79caf8cf6d0da535a63
carmour <callum.m.armour@gmail.com>
2019-04-21 22:33:59 -0700

    mohan engaged

--- Sprint-1.2/javascript/1.2-java.js
+++ Sprint-1.2/javascript/1.2-java.js
@@ -28,15 +28,19 @@ var ourUsers = [
     {
         name: "Michael Lyons",
         comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
-        date: "12/18/2018"
+        date: "12/18/2018",
+        photo: "bandsite-photos/band-img.jpg"
     }, {
         name: "Gary Wong",
         comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
-        date: "12/12/2018"
+        date: "12/12/2018",
+        photo: "bandsite-photos/band-img.jpg"
+
     }, {
         name: "Theodore Duncan",
         comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
-        date: "11/15/2018"
+        date: "11/15/2018",
+        photo: "bandsite-photos/band-img.jpg"
     }
 ];
 
@@ -45,7 +49,13 @@ function buildComment(commentInput) {
     
     // create photo div
     var photoDiv = document.createElement('div');
-    // TODO: add photo
+    var profilePhoto = document.createElement('img');
+    photoDiv.classList.add('photoDiv');
+    profilePhoto.classList.add('profilePhoto');
+    profilePhoto.src = commentInput.photo;
+    // add photo
+    photoDiv.appendChild(profilePhoto);
+    
     // create name div
     var nameDiv = document.createElement('div');
     nameDiv.classList.add('nameDiv');
@@ -72,7 +82,8 @@ function buildComment(commentInput) {
     var commentDiv = document.createElement('div');
     commentDiv.classList.add('commentDiv');
     commentDiv.appendChild(photoDiv);
-    commentDiv.appendChild(inputDiv);    
+    commentDiv.appendChild(inputDiv); 
+
     return commentDiv;    
 }
 
@@ -115,7 +126,8 @@ button.onclick = () => {
     var newEntry = {
         name: userName.value,
         comment: userComment.value,
-        date: mm+'/'+dd+'/'+yyyy
+        date: mm+'/'+dd+'/'+yyyy,
+        photo: "bandsite-photos/Mohan-muruge.jpg"
     }
     ourUsers.unshift(newEntry);
     commentContent.innerText = ''
